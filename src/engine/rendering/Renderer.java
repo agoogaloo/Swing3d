308f0d36421c5e5a2d515ef784655a48a0a302ba
samgcode <samgcode@gmail.com>
2023-10-27 13:11:14 -0600

    add translation
    - make triangles a list of 3 vertices instead of 9 numbers
    - add translate mesh function

--- src/engine/rendering/Renderer.java
+++ src/engine/rendering/Renderer.java
@@ -38,16 +38,12 @@ public class Renderer {
     for(int i = 0; i < meshes.length; i++) {
       Mesh mesh = meshes[i];
       for(int j = 0; j < mesh.triangles.length; j++) {
-        double[] triangle = mesh.triangles[j];
-        this.vertexPositions[j*3] = new double[] {
-          triangle[0], triangle[1], triangle[2]+3
-        };
-        this.vertexPositions[j*3+1] = new double[] {
-          triangle[3], triangle[4], triangle[5]+3
-        };
-        this.vertexPositions[j*3+2] = new double[] {
-          triangle[6], triangle[7], triangle[8]+3
-        };
+        double[][] triangle = mesh.triangles[j];
+        for (int k = 0; k < triangle.length; k++) {
+          this.vertexPositions[j*3+k] = new double[] {
+            triangle[k][0], triangle[k][1], triangle[k][2]
+          };
+        }
       }
     }
     
@@ -55,7 +51,7 @@ public class Renderer {
     renderPipeline.projectVertices();
     renderPipeline.applyVertexTransformations(vertexTransforms);
     renderPipeline.applyVertexShaders(vertexShaders);
-    renderPipeline.scan(true, true);
+    renderPipeline.scan(true, false);
     renderPipeline.applyFragmentShaders(fragmentShaders);
     renderPipeline.display(frame);
   }
--- src/engine/shapes/Mesh.java
+++ src/engine/shapes/Mesh.java
@@ -1,9 +1,20 @@
 package engine.shapes;
 
 public class Mesh {
-  public double[][] triangles;
+  public double[][][] triangles;
 
-  public Mesh(double[][] triangles) {
+  public Mesh(double[][][] triangles) {
     this.triangles = triangles;
+  }
+
+  public void translate(double[] distance) {
+    for(int j = 0; j < triangles.length; j++) {
+      double[][] triangle = triangles[j];
+      for (int k = 0; k < triangle.length; k++) {
+        triangle[k][0] += distance[0];
+        triangle[k][1] += distance[1];
+        triangle[k][2] += distance[2];
+      }
+    }
   }
 }
